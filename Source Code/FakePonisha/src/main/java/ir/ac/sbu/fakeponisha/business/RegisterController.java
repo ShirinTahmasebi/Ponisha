e8812b4fc22fd66fb32ee48618738079e3445e08
Shirin Tahmasebi <tahmasebi@arsh.co>
2016-05-19 03:11:54 +0430

    Validation on Registration form added

--- Source Code/FakePonisha/src/main/java/ir/ac/sbu/fakeponisha/business/RegisterController.java
+++ Source Code/FakePonisha/src/main/java/ir/ac/sbu/fakeponisha/business/RegisterController.java
@@ -3,6 +3,8 @@ package ir.ac.sbu.fakeponisha.business;
 import ir.ac.sbu.fakeponisha.model.User;
 import ir.ac.sbu.fakeponisha.persistance.UserDao;
 import ir.ac.sbu.fakeponisha.persistance.UserDaoImplementation;
+import ir.ac.sbu.fakeponisha.utils.Helper;
+import ir.ac.sbu.fakeponisha.utils.Regex;
 import ir.ac.sbu.fakeponisha.utils.Response;
 import ir.ac.sbu.fakeponisha.utils.Tag;
 import java.io.IOException;
@@ -19,47 +21,6 @@ import javax.servlet.http.HttpSession;
 public class RegisterController extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet RegisterController</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet RegisterController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. PostMethod is used to register users">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
      * Handles the HTTP <code>POST</code> method.
      *
      * @param request servlet request
@@ -72,13 +33,14 @@ public class RegisterController extends HttpServlet {
             throws ServletException, IOException {
         Response.initialize(response);
         HttpSession session = request.getSession();
-        String userName = new String(request.getParameter(Tag.USER_NAME).getBytes("8859_1"), "UTF-8");
-        String password = new String(request.getParameter(Tag.PASSWORD).getBytes("8859_1"), "UTF-8");
-        String email = new String(request.getParameter(Tag.EMAIL).getBytes("8859_1"), "UTF-8");
+        String userName = Helper.getRequestString(request, Tag.USER_NAME);
+        String password = Helper.getRequestString(request, Tag.PASSWORD);
+        String email = Helper.getRequestString(request, Tag.EMAIL);
 
         User user = new User();
         user.setUsername(userName);
         user.setPassword(password);
+        user.setEmail(email);
 
         response.setContentType("text/html;charset=UTF-8");
         String forwardPage = checkInsertUser(user);
@@ -103,15 +65,31 @@ public class RegisterController extends HttpServlet {
     }
 
     private String checkInsertUser(User user) {
+        boolean isUserValid = checkValidUserInfoFormat(user);
+        boolean isUserUnique = (isUserValid) ? checkUniqueUserInfo(user) : false;
+
+        if (isUserValid && isUserUnique) {
+            return Tag.FIRST_PAGE;
+        }
+        return Tag.REGISTER_PAGE;
+
+    }
+
+    private boolean checkValidUserInfoFormat(User user) {
+        boolean isUsernameValid = Helper.validator(user.getUsername(), Regex.USERNAME_PATTERN);
+        boolean isPasswordValid = Helper.validator(user.getPassword(), Regex.PASSWORD_PATTERN);
+        boolean isEmailValid = Helper.validator(user.getEmail(), Regex.EMAIL_PATTERN);
+        return isUsernameValid && isPasswordValid && isEmailValid;
+    }
+
+    private boolean checkUniqueUserInfo(User user) {
         UserDao userDao = new UserDaoImplementation();
-        User u;
-        u = userDao.getUser(user.getUsername());
+        User u = userDao.getUser(user.getUsername());
         if (u == null) {
             userDao.insertUser(user);
-            return Tag.FIRST_PAGE;
-        } else {
-            return Tag.REGISTER_PAGE;
+            return true;
         }
+        return false;
     }
 
 }
--- Source Code/FakePonisha/target/generated-sources/annotations/ir/ac/sbu/fakeponisha/model/User_.java
+++ Source Code/FakePonisha/target/generated-sources/annotations/ir/ac/sbu/fakeponisha/model/User_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-17T04:48:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-19T03:09:30")
 @StaticMetamodel(User.class)
 public class User_ { 
 
