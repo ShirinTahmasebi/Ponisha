0509995690b46b3bd657a0059dcbfb5bd30f66f6
Shirin Tahmasebi <tahmasebi@arsh.co>
2016-05-21 20:39:02 +0430

    Final Version For Phase 2

--- Source Code/FakePonisha/target/generated-sources/annotations/ir/ac/sbu/fakeponisha/model/Company_.java
+++ Source Code/FakePonisha/target/generated-sources/annotations/ir/ac/sbu/fakeponisha/model/Company_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-21T20:20:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-21T20:36:18")
 @StaticMetamodel(Company.class)
 public class Company_ { 
 
--- Source Code/FakePonisha/target/generated-sources/annotations/ir/ac/sbu/fakeponisha/model/Resume_.java
+++ Source Code/FakePonisha/target/generated-sources/annotations/ir/ac/sbu/fakeponisha/model/Resume_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-21T20:20:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-21T20:36:18")
 @StaticMetamodel(Resume.class)
 public class Resume_ { 
 
